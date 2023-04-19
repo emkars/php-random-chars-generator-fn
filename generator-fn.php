a8dd8615f64878f6a5fd92841983795979454d97
Emrah Kars <grafimed@gmail.com>
2023-04-19 14:56:26 +0300

    Create generator-fn.php
    Random char generator fn initial commit

---
+++ generator-fn.php
@@ -0,0 +1,13 @@
+ <?php
+  
+  // varsayılanda 10 karakter ile oluşturacaktır
+
+  function random($charCount = 10)  {
+		$char="abcdefghijklmnoprstuwvyzqx1234567890";
+		$s = "";
+		for ($k=1;$k<=$charCount;$k++) {
+			$h=substr($char,mt_rand(0,strlen($char)-1),1);
+			$s.=$h;
+		}
+		return $s;
+	}
